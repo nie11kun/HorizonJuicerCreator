0b29e1fa007012919d2117743a8f729321ecdb6c
Marco Nie <github@niekun.net>
2022-02-24 11:24:18 +0800

    add stylesheet file

--- main.cpp
+++ main.cpp
@@ -6,6 +6,13 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     HorizonJuicerCreator w;
+
+    QFile file(":./stylesheet.qss");
+    file.open(QFile::ReadOnly);
+    QString styleSheet = QLatin1String(file.readAll());
+    file.close();
+    a.setStyleSheet(styleSheet);
+
     w.show();
     return a.exec();
 }

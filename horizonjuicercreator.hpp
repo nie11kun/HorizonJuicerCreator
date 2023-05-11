e280c8d5cf208cc243ffcba0ac8f04b8240dd887
Marco Nie <github@niekun.net>
2023-05-11 11:03:11 +0800

    update

--- horizonjuicercreator.hpp
+++ horizonjuicercreator.hpp
@@ -43,6 +43,6 @@ private:
     History *history;
     void setVersion();
     ReadAndWriteJson *r;
-    QString version = "3.16.1";
+    QString version = "3.16.2";
 };
 #endif // HORIZONJUICERCREATOR_H
